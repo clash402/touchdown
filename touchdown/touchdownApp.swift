2d9eddd810ddbf786bc6038adb02c0eb6f25537c
Josh Courtney <joshrcourtney@gmail.com>
2021-05-28 12:46:35 -0500

    Initial Commit

---
+++ touchdown/touchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  touchdownApp.swift
+//  touchdown
+//
+//  Created by Josh Courtney on 5/28/21.
+//
+
+import SwiftUI
+
+@main
+struct touchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
