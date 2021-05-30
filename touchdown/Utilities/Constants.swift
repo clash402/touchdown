39e9e8df31b314ed527718393377b8e65a84ba38
Josh Courtney <joshrcourtney@gmail.com>
2021-05-30 06:05:19 -0500

    Setup project

---
+++ touchdown/Utilities/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  touchdown
+//
+//  Created by Josh Courtney on 5/28/21.
+//
+
+import SwiftUI
+
+let colorBackground = Color("ColorBackground")
+let colorGray = Color(UIColor.systemGray4)
